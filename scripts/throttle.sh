da7d2a5bcb9b101a83a0dbcb6cc17c2f956502ee
Tanjina <tanjina012@gmail.com>
2025-06-30 11:40:06 +0000

    Set bandwidth between client and server to 100Mbps

--- scripts/throttle.sh
+++ scripts/throttle.sh
@@ -17,7 +17,8 @@ then
 sudo tc qdisc del dev $DEV root
 ## about 3Gbps
 #sudo tc qdisc add dev $DEV root handle 1: tbf rate 3000mbit burst 100000 limit 10000
-sudo tc qdisc add dev $DEV root handle 1: tbf rate 500mbit burst 100000 limit 10000 # Tanjina-note: about 500Mbps
+# sudo tc qdisc add dev $DEV root handle 1: tbf rate 500mbit burst 100000 limit 10000 # Tanjina-note: about 500Mbps
+sudo tc qdisc add dev $DEV root handle 1: tbf rate 100mbit burst 100000 limit 10000 # Tanjina-note: about 100Mbps
 ## about 0.3ms ping latency
 sudo tc qdisc add dev $DEV parent 1:1 handle 10: netem delay 0.15msec
 fi
@@ -26,7 +27,8 @@ then
 sudo tc qdisc del dev $DEV root
 ## about 400Mbps
 #sudo tc qdisc add dev $DEV root handle 1: tbf rate 400mbit burst 100000 limit 10000
-sudo tc qdisc add dev $DEV root handle 1: tbf rate 500mbit burst 100000 limit 10000 # Tanjina-note: about 500Mbps
+# sudo tc qdisc add dev $DEV root handle 1: tbf rate 500mbit burst 100000 limit 10000 # Tanjina-note: about 500Mbps
+sudo tc qdisc add dev $DEV root handle 1: tbf rate 100mbit burst 100000 limit 10000 # Tanjina-note: about 100Mbps
 ## about 40ms ping latency
 sudo tc qdisc add dev $DEV parent 1:1 handle 10: netem delay 20msec
 fi
