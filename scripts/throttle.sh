6e99bd8f68f635033cc9727324b9edd63aa76229
Tanjina <tanjina012@gmail.com>
2025-09-12 11:07:25 +0000

    Added power measurement code in SqueezeNet, ResNet50, and DenseNet121 for SCI_HE

--- scripts/throttle.sh
+++ scripts/throttle.sh
@@ -16,11 +16,17 @@ if [ "$1" == "lan" ]
 then
 sudo tc qdisc del dev $DEV root
 ## about 3Gbps
-#sudo tc qdisc add dev $DEV root handle 1: tbf rate 3000mbit burst 100000 limit 10000
+# sudo tc qdisc add dev $DEV root handle 1: tbf rate 3000mbit burst 100000 limit 10000
+sudo tc qdisc add dev $DEV root handle 1: tbf rate 900mbit burst 100000 limit 10000 # Tanjina-note: about 900Mbps
+# sudo tc qdisc add dev $DEV root handle 1: tbf rate 800mbit burst 100000 limit 10000 # Tanjina-note: about 800Mbps
+# sudo tc qdisc add dev $DEV root handle 1: tbf rate 700mbit burst 100000 limit 10000 # Tanjina-note: about 700Mbps
+# sudo tc qdisc add dev $DEV root handle 1: tbf rate 600mbit burst 100000 limit 10000 # Tanjina-note: about 600Mbps
 # sudo tc qdisc add dev $DEV root handle 1: tbf rate 500mbit burst 100000 limit 10000 # Tanjina-note: about 500Mbps
-# sudo tc qdisc add dev $DEV root handle 1: tbf rate 100mbit burst 100000 limit 10000 # Tanjina-note: about 100Mbps
+# sudo tc qdisc add dev $DEV root handle 1: tbf rate 400mbit burst 100000 limit 10000 # Tanjina-note: about 400Mbps
+# sudo tc qdisc add dev $DEV root handle 1: tbf rate 300mbit burst 100000 limit 10000 # Tanjina-note: about 300Mbps
 # sudo tc qdisc add dev $DEV root handle 1: tbf rate 200mbit burst 100000 limit 10000 # Tanjina-note: about 200Mbps
-sudo tc qdisc add dev $DEV root handle 1: tbf rate 50mbit burst 100000 limit 10000 # Tanjina-note: about 50Mbps
+# sudo tc qdisc add dev $DEV root handle 1: tbf rate 100mbit burst 100000 limit 10000 # Tanjina-note: about 100Mbps
+# sudo tc qdisc add dev $DEV root handle 1: tbf rate 50mbit burst 100000 limit 10000 # Tanjina-note: about 50Mbps
 ## about 0.3ms ping latency
 sudo tc qdisc add dev $DEV parent 1:1 handle 10: netem delay 0.15msec
 fi
