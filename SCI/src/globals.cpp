712a3968d9dd2aa70ed8940bdf65ef81b7b0c29d
Tanjina <tanjina012@gmail.com>
2026-01-23 13:22:21 +0000

    updated path to the power monitoring sensor

--- SCI/src/globals.cpp
+++ SCI/src/globals.cpp
@@ -138,7 +138,8 @@ int AvgPool_layer_count = 0;
 int ArgMax_layer_count = 0;
 
 // Path to the power usage
-string power_usage_path = "/sys/class/hwmon/hwmon3/device/power1_average"; 
+// string power_usage_path = "/sys/class/hwmon/hwmon3/device/power1_average"; 
+string power_usage_path = "/sys/class/hwmon/hwmon4/device/power1_average"; 
 
 // Added by Tanjina
 double computeAveragePower(uint64_t totalPower, int layerCount, const std::string& layerName){
